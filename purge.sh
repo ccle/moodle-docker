6338f4609231beae2c9467edfcfc0fb251bbecba
willyjlee <williamjlee@ucla.edu>
2017-09-08 17:09:33 -0700

    Will + Amit partnership commit: Fixed bugs in the purge script and updated the README accordingly

--- purge.sh
+++ purge.sh
@@ -1,18 +1,27 @@
-# Small cmd line arg convenience function
-function arg_exists {
-    for a in ${BASH_ARGV[*]}; do
-	if [ "$a" == "$1" ]; then
-	    return 1
-	fi
-    done
-    return 0
-}
-
 # Check some cmd line args
+
+volumes=false
+images=false
+untagged=false
+for a in ${BASH_ARGV[*]}; do
+    if [ "$a" == "--volumes" ]; then
+	volumes=true
+    elif [ "$a" == "--images" ]; then
+	images=true
+    elif [ "$a" == "--untagged" ]; then
+	untagged=true
+    elif [ "$a" == "--all" ]; then
+	volumes=true
+	images=true
+	untagged=true
+    fi
+done
+
 rmflag=''
-printflag=' and all its volumes'
-if [ $(arg_exists "--volumes") ]; then
-    $rmflag='-v'
+printflag=''
+if [ "$volumes" = true ]; then
+    printflag=' and all its volumes'
+    rmflag='-v'
 fi
 
 containers=("moodledocker_webserver_1" "moodledocker_selenium_1" "moodledocker_mailhog_1" "moodledocker_phpmyadmin_1" "moodledocker_db_1")
@@ -24,14 +33,14 @@ for i in ${containers[*]}; do
     sudo docker rm $rmflag $i
 done
 
-images=("moodlehq/moodle-php-apache:7.0" "phpmyadmin/phpmyadmin" "mysql:5" "mailhog/mailhog" "selenium/standalone-firefox:2.53.1")
+imagenames=("moodlehq/moodle-php-apache:7.0" "phpmyadmin/phpmyadmin" "mysql:5" "mailhog/mailhog" "selenium/standalone-firefox:2.53.1")
 # Delete the relevant images
-if [ $(arg_exists "--images") ]; then
-    for i in ${images[*]}; do
+if [ "$images" = true ]; then
+    for i in ${imagenames[*]}; do
 	echo "Removing image $i..."
 	sudo docker rmi $i
     done
-    if [ $(arg_exists "--untagged") ]; then
+    if [ $untagged ]; then
 	echo "Removing all untagged images..."
 	sudo docker rmi $(sudo docker images -a | grep "^<none>" | awk '{print $3}')
     fi
@@ -40,11 +49,11 @@ fi
 # Show that everything has been removed
 echo "Showing containers (sudo docker ps -a)..."
 sudo docker ps -a
-if [ $(arg_exists "--images") ]; then
+if [ "$images" = true ]; then
     echo "Showing images (sudo docker images -a)..."    
     sudo docker images -a
 fi
-if [ $(arg_exists "--volumes") ]; then
+if [ "$volumes" = true ]; then
     echo "Showing volumes (sudo docker volume ls)"
     sudo docker volume ls
 fi
