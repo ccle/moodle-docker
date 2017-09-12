34c1ebbfa81ac779f0502ee0343d57ff8cf3335c
Rex Lorenzo <rex@oid.ucla.edu>
2017-09-11 18:28:52 -0700

    Adding back config settings that enable Behat and PHPunit.

--- moodle-docker_config_private-dist.php
+++ moodle-docker_config_private-dist.php
@@ -5,22 +5,11 @@
 $CFG->dbtype    = getenv('MOODLE_DOCKER_DBTYPE');
 $CFG->dblibrary = 'native';
 $CFG->dbhost    = 'db';
-
-
 $CFG->dbname    = getenv('MOODLE_DOCKER_DBNAME');
 $CFG->dbuser    = getenv('MOODLE_DOCKER_DBUSER');
 $CFG->dbpass    = getenv('MOODLE_DOCKER_DBPASS');
-
-// Registrar
-$CFG->registrar_dbtype = 'odbc_mssql';
-$CFG->registrar_dbhost = '';
-$CFG->registrar_dbuser = '';
-$CFG->registrar_dbpass = '';
-$CFG->registrar_dbname = '';
-
 $CFG->prefix    = 'mdl_';
 $CFG->dboptions = ['dbcollation' => getenv('MOODLE_DOCKER_DBCOLLATION')];
-
 
 $CFG->wwwroot   = 'http://localhost';
 $port = getenv('MOODLE_DOCKER_WEB_PORT');
@@ -31,3 +20,55 @@ $CFG->dataroot  = '/var/www/moodledata';
 $CFG->admin     = 'admin';
 $CFG->directorypermissions = 0777;
 $CFG->smtphosts = 'mailhog:1025';
+
+// Debug options - possible to be controlled by flag in future..
+$CFG->debug = (E_ALL | E_STRICT); // DEBUG_DEVELOPER
+$CFG->debugdisplay = 1;
+$CFG->debugstringids = 1; // Add strings=1 to url to get string ids.
+$CFG->perfdebug = 15;
+$CFG->debugpageinfo = 1;
+$CFG->allowthemechangeonurl = 1;
+$CFG->passwordpolicy = 0;
+
+$CFG->phpunit_dataroot  = '/var/www/phpunitdata';
+$CFG->phpunit_prefix = 't_';
+$CFG->behat_wwwroot   = 'http://webserver';
+$CFG->behat_dataroot  = '/var/www/behatdata';
+$CFG->behat_prefix = 'b_';
+$CFG->behat_profiles = array(
+    'default' => array(
+        'browser' => getenv('MOODLE_DOCKER_BROWSER'),
+        'wd_host' => 'http://selenium:4444/wd/hub',
+    ),
+);
+$CFG->behat_faildump_path = '/var/www/behatfaildumps';
+define('PHPUNIT_LONGTEST', true);
+if (getenv('MOODLE_DOCKER_PHPUNIT_EXTRAS')) {
+    define('TEST_SEARCH_SOLR_HOSTNAME', 'solr');
+    define('TEST_SEARCH_SOLR_INDEXNAME', 'test');
+    define('TEST_SEARCH_SOLR_PORT', 8983);
+    define('TEST_SESSION_REDIS_HOST', 'redis');
+    define('TEST_CACHESTORE_REDIS_TESTSERVERS', 'redis');
+    define('TEST_CACHESTORE_MONGODB_TESTSERVER', 'mongodb://mongo:27017');
+    define('TEST_CACHESTORE_MEMCACHED_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
+    define('TEST_CACHESTORE_MEMCACHE_TESTSERVERS', "memcached0:11211\nmemcached1:11211");
+    define('TEST_LDAPLIB_HOST_URL', 'ldap://ldap');
+    define('TEST_LDAPLIB_BIND_DN', 'cn=admin,dc=openstack,dc=org');
+    define('TEST_LDAPLIB_BIND_PW', 'password');
+    define('TEST_LDAPLIB_DOMAIN', 'ou=Users,dc=openstack,dc=org');
+    define('TEST_AUTH_LDAP_HOST_URL', 'ldap://ldap');
+    define('TEST_AUTH_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
+    define('TEST_AUTH_LDAP_BIND_PW', 'password');
+    define('TEST_AUTH_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
+    define('TEST_ENROL_LDAP_HOST_URL', 'ldap://ldap');
+    define('TEST_ENROL_LDAP_BIND_DN', 'cn=admin,dc=openstack,dc=org');
+    define('TEST_ENROL_LDAP_BIND_PW', 'password');
+    define('TEST_ENROL_LDAP_DOMAIN', 'ou=Users,dc=openstack,dc=org');
+}
+
+// Registrar
+$CFG->registrar_dbtype = 'odbc_mssql';
+$CFG->registrar_dbhost = '';
+$CFG->registrar_dbuser = '';
+$CFG->registrar_dbpass = '';
+$CFG->registrar_dbname = '';
